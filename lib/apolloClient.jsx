b31afba97a7d2e6979ce6a2c32eb3cb8ee9fc8de
Rodrigo Toledo <rodrigo@rtoledo.inf.br>
2024-10-29 13:24:18 -0300

    Add Apollo Client and GraphQL dependencies

---
+++ lib/apolloClient.jsx
@@ -0,0 +1,23 @@
+import { ApolloClient, InMemoryCache, HttpLink } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+
+const httpLink = new HttpLink({
+  uri: process.env.NEXT_PUBLIC_API_BASE_URL,
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
+
+export default client;
